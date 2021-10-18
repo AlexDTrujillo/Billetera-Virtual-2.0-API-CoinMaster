05eddd62a581a6bb62c62d39b7817f5944a30a4c
AlexD.Trujillo <alexdanii1992@gmail.com>
2021-10-17 21:38:14 -0500

    agregado historial en archivo texto

--- Prototipo billetera coinmaker.py
+++ Prototipo billetera coinmaker.py
@@ -36,14 +36,16 @@ totalDolares=0
 historiaSalida=[]
 historiaEntrada=[]
 historiaFondeo=[]
+nombre_archivo = "Historial.txt" 
 
-#inicio solicitando codigo
+
+#inicio solicitando codigo del propietario
 codigoPropietario=input("para iniciar, por favor ingrese su codigo de usuario: ")
-#Cuerpo del programa (continuo hasta que se elija el 7)
+#Menu del programa (continuo hasta que se elija el 7)
 while not seleccionMenu=="7":
     seleccionMenu=input("\nElija una opcion, marcando un numero del siguiente Menu: \n1-Recibir Transferencia \n2-Hacer Tranferencia  \n3-Conoce tu balance de una criptomoneda \n4-Balance general \n5-historial de transacciones \n6-Agregar fondos a tu billetera \n7-salir del programa \n")
 
-    #Recibir Cripto
+    # opcion 1: Recibir Cripto
     if seleccionMenu=="1":
         codigoRemitente=input("Codigo de quien envia las criptomonedas: ")
         if codigoRemitente!=codigoPropietario:
@@ -54,10 +56,10 @@ while not seleccionMenu=="7":
             estadoTranferencia=estado1
             ahora= datetime.now (tz = None)
             fecha=ahora.strftime("%A, %d de %B de %Y a las %I:%M:%S%p")
-        historiaEntrada.append([codigoRemitente,symbol,cantCriptoRecivir,estadoTranferencia,fecha])
+        historiaEntrada.append(str([codigoRemitente,symbol,cantCriptoRecivir,estadoTranferencia,fecha]))
         symbol=False
 
-    #Enviar symbol
+    #opcion 2:Enviar symbol
     if seleccionMenu=="2":
         codigoDestinatario=input("Codigo de quien recibe las criptomonedas: ")
         if codigoDestinatario!=codigoPropietario:
@@ -72,10 +74,10 @@ while not seleccionMenu=="7":
                 estadoTranferencia=estado1
             ahora= datetime.now (tz = None)
             fecha=ahora.strftime("%A, %d de %B de %Y a las %I:%M:%S%p")
-            historiaSalida.append([codigoDestinatario,symbol,cantCriptoEnviar,estadoTranferencia,fecha])
+            historiaSalida.append(str([codigoDestinatario,symbol,cantCriptoEnviar,estadoTranferencia,fecha]))
             symbol=False
 
-    #Balance una Moneda
+    #opcion 3: Balance una Moneda
     if seleccionMenu=="3":
         while not esCriptomoneda(symbol):
             symbol = input("¿Criptomoneda a ver en tu Balance?,\npor favor usar las siguientes abreviaciones (BTC, BCC,LTC,ETH,ETC,XRP: ")
@@ -86,7 +88,7 @@ while not seleccionMenu=="7":
         valorDolares=contenidoBilletera[symbol]*cambio
         print("Tus ",symbol," te representan un saldo de ", valorDolares,"dolares\n")
 
-    #Balance General
+    #opcion 4: Balance General
     if seleccionMenu=="4":
         print("Actualmente tienes en tu billetera: \n",contenidoBilletera)
         for symbol in contenidoBilletera:
@@ -97,19 +99,35 @@ while not seleccionMenu=="7":
             totalDolares=totalDolares+ valorDolares        
         print("Tus criptomonedas te representan un saldo total de ", totalDolares,"dolares\n")
 
-    #Historial de transacciones
+    #opcion 5: Historial de transacciones
     if seleccionMenu=="5":
-        print("Has Hecho las siguientes tranferencias de salida desde tu billetera: ")
+        #impresion de historial en archivo txt
+        archivo = open(nombre_archivo,"w")
+        archivo.write("\n"+"Has Hecho las siguientes tranferencias de salida desde tu billetera: "+"\n")
+        archivo.write("Destinatario, Symbol, Valor transferencia, Estado transferencia, Fecha "+"\n")
+        for salidas in historiaSalida:
+            archivo.writelines(salidas+"\n")
+        archivo.write("\n"+"Has recibido las siguientes tranferencias a tu billetera: "+"\n")
+        archivo.write("Remitente, Symbol, Valor transferencia, Estado transferencia, Fecha "+"\n")
+        for entradas in historiaEntrada:
+            archivo.writelines(entradas+"\n")
+        archivo.write("\n"+"Has realizado los siguientes depositos de tu parte a tu billetera: "+"\n")
+        archivo.write("Symbol, Valor transferencia, Estado transferencia, Fecha "+"\n")
+        for fondos in historiaFondeo:
+            archivo.writelines(fondos+"\n")
+        archivo.close()
+        #impresion de historial en pantalla
+        print("Has Hecho las siguientes tranferencias de salida desde tu billetera: "+"\n")
         for salidas in historiaSalida:
             print(salidas)
-        print("Has recibido las siguientes tranferencias a tu billetera: ")
+        print("Has recibido las siguientes tranferencias a tu billetera: "+"\n")
         for entradas in historiaEntrada:
             print(entradas)
-        print("Has realizado los siguientes depositos de tu parte a tu billetera: ")
+        print("Has realizado los siguientes depositos de tu parte a tu billetera: "+"\n")
         for fondos in historiaFondeo:
             print(fondos)
-    
-    #agregar fondos
+     
+    #opcion 6: agregar fondos 
     if  seleccionMenu=="6":
         while not esCriptomoneda(symbol):
             symbol = input("¿criptomoneda a agregar fondos?,\npor favor usar las siguientes abreviaciones BTC, BCC,LTC,ETH,ETC,XRP: ")
@@ -118,9 +136,10 @@ while not seleccionMenu=="7":
         estadoTranferencia=estado1
         ahora= datetime.now (tz = None)
         fecha=ahora.strftime("%A, %d de %B de %Y a las %I:%M:%S%p")
-        historiaFondeo.append(["Fondos Agregados",symbol,cantCriptoFondear,estadoTranferencia,fecha])
+        historiaFondeo.append(str([symbol,cantCriptoFondear,estadoTranferencia,fecha]))
         symbol=False
 
     #Salida del programa
     if seleccionMenu=="7":
+        #open(nombre_archivo, 'w').close()#Borrar contenido del documento historial, para usarlo limpio al volver a abrir
         exit()
